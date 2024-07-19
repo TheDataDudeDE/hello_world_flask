3350399dccd582e732ab8a12e94c454f448e80a8
Markus (Abdullah) Clauss <markus@thedatadude.de>
2024-07-19 17:44:05 +0200

    Another round

--- app.py
+++ app.py
@@ -6,7 +6,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def hello_world():
-    return "Hello, World from Azure! Reloaded"
+    return "<H1>Hello, World from Azure! Reloaded</H1>"
 
 
 if __name__ == "__main__":
